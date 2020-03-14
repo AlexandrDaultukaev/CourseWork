9305f76e66f1089ab492958ffeab0f7f913d355c
Kluishnichenko Alex <babosiki228@gmail.com>
2020-03-14 14:11:18 +0700

    Add search random line and print it, chek correctly

--- kurs.c
+++ kurs.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
+int getrand(int min, int max)
+{
+	srand(time(NULL));
+	return (double)rand() / (RAND_MAX + 1.0) * (max - min) + min;
+}
 
 int begin(char run[4]) {
     while (strcmp(run, "run") != 0) {
@@ -10,13 +17,13 @@ int begin(char run[4]) {
     return 0;
 } 
 
-void print(correct, uncorrect) {
+void print(int correct, int uncorrect) {
     printf("Correct words: %d\nUncorrect words: %d\n", correct, uncorrect);
     exit(0);
 }
 
 int main() {
-    typedef struct {
+    /*typedef struct {
 	char name[20];
     }type;
     char run[4];
@@ -46,6 +53,43 @@ int main() {
 	i++;
 	     }
 	print(correct, uncorrect);
+	}*/
+	FILE *fp;
+    char source[30], input[10];
+	char sep[5] = " \n";
+	char *istr;
+	int i = 0,r,yes = 0,j = 0,n;
+    fp = fopen("input.txt","r");
+	printf("Amount words\n");
+	scanf("%d",&n);
+	//fgets(source, 20, fp);
+   // scanf("%s", &input);
+	while (j != n)
+	{
+		r = getrand(1,60);
+		//printf("%d\n",r);
+		fseek(fp,0,SEEK_SET);
+		i = 0;
+		while (i != r)
+		{
+			if (fgets(source,30,fp) != NULL)
+			{
+				//printf("%s\n",source);
+				i++;
+			}
+		}
+			istr = strtok(source,sep);
+			printf("%s\n",istr);
+			scanf("%s",input);
+			if (strcmp(istr,input) == 0)
+				yes++;
+			if (strcmp(istr,input) != 0)
+				printf("%s\n",istr);
+			j++;
 	}
+		//istr = strtok(NULL,sep);
+		//printf("s: %s\n",istr);
+	printf("%d\n",yes);
+	fclose(fp);
   return 0;
 }
